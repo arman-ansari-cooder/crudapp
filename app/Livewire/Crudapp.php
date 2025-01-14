adfd5e2d8563c32ae181936a6e9befa243a9a3d8
ArmanAnsari <armanansari62010@gmail.com>
2025-01-14 01:25:41 +0000

    the data store in table of category data

--- app/Livewire/Crudapp.php
+++ app/Livewire/Crudapp.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+
 
 class Crudapp extends Component
 {
@@ -16,6 +18,9 @@ class Crudapp extends Component
 
         $this->validate([
             'category' => 'required|string|min:3|max:255', 
+        ]);
+        Category::create([
+            'category' => $this->category,  // Save the input value
         ]);
 
         session()->flash('message', 'Category submitted successfully.');
